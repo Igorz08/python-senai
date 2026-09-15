00eaddd01816f15f3bdc326c26041c44e968ab78
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-09-15 14:29:51 -0300

    feat: adicionando literais

---
+++ Definicao-Literal/literais.py
@@ -0,0 +1,15 @@
+# String
+nome = "Igor"
+
+# Números inteiros e decimais
+idade = 18
+altura = 1.75
+
+# Valor lógico (true e false)
+professor = False
+
+# Verifica a tpagem das variaveis
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(professor))
