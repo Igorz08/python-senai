8ff84bd23b0c4badeea5fa42c7f04991508f80b3
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-09-21 15:56:53 -0300

    feat: adicionando for

---
+++ 04-Estrutura-de-repeticao/06-exercicio-par-impar.py
@@ -0,0 +1,6 @@
+for numero in range(1 , 11):
+
+    if numero % 2 == 0:
+        print(f"O número {numero} é par")
+    else:
+        print(f"O número {numero} é impar")
---
+++ 04-Estrutura-de-repeticao/07-for-exercicio-tabuada.py
@@ -0,0 +1,15 @@
+# Cria um programa que peça um número ao utilizador e mostre a 
+# tabuada desse número de 1 a 10, utilizando um ciclo for.
+
+# Digite um número: 5
+
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# ...
+# 5 x 10 = 50
+
+num = int(input("Digite um número: "))
+
+for tabuada in range(1, 11):
+    print(f"{num} x {tabuada} = {num * tabuada}")
