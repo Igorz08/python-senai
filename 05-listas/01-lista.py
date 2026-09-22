a73cc20d2495e53a71f57fa3aa233b7d26c9741d
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-09-22 14:54:07 -0300

    feat: adicionando exercicio de lista de times

---
+++ 05-listas/01-lista.py
@@ -0,0 +1,10 @@
+# Cria uma lista de frutas
+frutas = ["Maça", "Banana", "Laranja"]
+
+# Exibe a lista
+print(frutas)
+
+# Como acessar um item
+print(frutas[0]) #maça
+print(frutas[2]) #laranja
+print(frutas[1]) #banana
